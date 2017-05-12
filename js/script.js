4811891bd70437700e8ca4a5a77617f01a18f821
Alexandre Fonseca Faria <alexandreffaria@me.com>
2017-05-12 00:43:15 -0300

    Now it generetes an array of all the students names. And some variables were changed to a global scope.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 //Pagination & Content Filter
 
-setTimeout(showPage, 1000); // just a caller for testing;
+var students = document.getElementsByClassName('student-item');
+var studentsNames = document.querySelectorAll('.student-details h3');
+var numberOfPages = Math.ceil(students.length/10);
 
 
-function showPage(pageNumber, studentList){ // Displays a list of 10 students based on page-number selected
-  var students = document.getElementsByClassName('student-item');
 
+
+function showPage(pageNumber, studentsList){ // Displays a list of 10 students based on page-number selected
 
   for(i = 10; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
     students[i].style.display = 'none';  // after the first 10
   }
-
-
 
   pagination();
 }
@@ -20,10 +20,17 @@ function appendPageLinks() { // Creates the links to the different lists
 
 }
 
+function createList() {
+
+  var studentsList = [];
+  for(i = 0 ; i < studentsNames.length ; i++){
+
+  }
+  return studentsList;
+}
 
 function pagination() {
-  var students = document.getElementsByClassName('student-item');
-  var numberOfPages = Math.ceil(students.length/10) ;
+
   var paginationArea = document.querySelector('.pagination');
 
   var html = '<ul>';
@@ -41,3 +48,6 @@ $('.pagination').click( () => { // Listen to the pagination for filtering purpos
     console.log(':::',test);
 
   });
+
+
+setTimeout(showPage, 1000); // just a caller for testing;
