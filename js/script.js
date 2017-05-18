763c2ec3960bd52dc4f852b30bbe8fc6e4f02189
Alexandre Fonseca Faria <alexandreffaria@me.com>
2017-05-18 16:53:00 -0300

    showPage is listening to pagination links

--- js/script.js
+++ js/script.js
@@ -7,11 +7,23 @@ var paginationArea = document.querySelector('.pagination');
 
 
 var listenToPage = function appendPageLinks(listOfStudents) { /* take a student list as an argument */
-  var html = pagination();
+  var numberOfPages = Math.ceil(students.length/10); // determine how many pages for this student list
+  var html = '<ul>'; // create a page link section
 
-    paginationArea.innerHTML = '';
+  for(i = 1 ; i< numberOfPages ; i++){  // “for” every page add a page link to the page link section
+    html += '<li><a href="#">' + i + '</a></li>';
+  }
+  html += '</ul>';
+  paginationArea.innerHTML = html;
 
-  showPage(1);
+
+  $('.pagination a').click(function(){
+    $(this).toggleClass('active');
+    showPage(this, listOfStudents);
+  })
+
+
+
 };
 
 
@@ -21,40 +33,16 @@ function showPage(pageNumber, studentsList){ // Displays a list of 10 students b
   // Then loop through all students in our student list argument
       // if student should be on this page number
           // show the student
-
-
-
   var superiorLimit = pageNumber*10;
 
   for(i = 0; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
     students[i].style.display = 'none';
   }
-
   // for(i = 0; i < studentsList.length; i++){ // This loops through all students in our student list argument
   //   //if(studentsList.name ===)
   //
   // }
-
-
-
 }
 
 
-function pagination() {
-  var numberOfPages = Math.ceil(students.length/10);
-
-
-  var html = '<ul>';
-
-  for(i = 1 ; i< numberOfPages ; i++){
-    html += '<li><a href="#">' + i + '</a></li>';
-  }
-  html += '</ul>';
-
-  return html;
-}
-
-
-
-  debugger;
 setTimeout(listenToPage(students), 3000); // just a caller for testing;
