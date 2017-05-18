6ed2a03c0adf2e1df6818944fb644cf47d513309
Alexandre Fonseca Faria <alexandreffaria@me.com>
2017-05-18 19:16:52 -0300

    Pagination is working but the fact that all the students are loaded until any link is cliked.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 //Pagination & Content Filter
 
+
 var students = document.getElementsByClassName('student-item');
 var studentsNames = document.querySelectorAll('.student-details h3');
 var paginationArea = document.querySelector('.pagination');
 
-
+$( document ).ready(function() {
+    listenToPage(students);
+    $(".pagination li:first-child a").addClass("active");
+});
 
 var listenToPage = function appendPageLinks(listOfStudents) { /* take a student list as an argument */
   var numberOfPages = Math.ceil(students.length/10); // determine how many pages for this student list
   var html = '<ul>'; // create a page link section
 
-  for(i = 1 ; i< numberOfPages ; i++){  // “for” every page add a page link to the page link section
+  for(var i = 1 ; i< numberOfPages ; i++){  // “for” every page add a page link to the page link section
     html += '<li><a href="#">' + i + '</a></li>';
   }
   html += '</ul>';
@@ -18,31 +22,29 @@ var listenToPage = function appendPageLinks(listOfStudents) { /* take a student 
 
 
   $('.pagination a').click(function(){
-    $(this).toggleClass('active');
-    showPage(this, listOfStudents);
+
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+    showPage(this.text, listOfStudents);
+
   })
-
-
 
 };
 
 
 
-function showPage(pageNumber, studentsList){ // Displays a list of 10 students based on page-number selected
-  // first hide all students on the page. CHECK
-  // Then loop through all students in our student list argument
-      // if student should be on this page number
-          // show the student
-  var superiorLimit = pageNumber*10;
+function showPage(pageNumber, studentsList){
 
-  for(i = 0; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
+  var superiorLimit = pageNumber*10;
+  var inferiorLimit = superiorLimit - 9;
+
+  for(i = 0; i < studentsList.length; i++){ // This for just hide everyone on the page using th stunde-iten class
     students[i].style.display = 'none';
   }
-  // for(i = 0; i < studentsList.length; i++){ // This loops through all students in our student list argument
-  //   //if(studentsList.name ===)
-  //
-  // }
+
+   for( ; superiorLimit >= inferiorLimit; superiorLimit--){
+     console.log(superiorLimit);
+     students[superiorLimit].style.display = 'block';
+
+   }
 }
-
-
-setTimeout(listenToPage(students), 3000); // just a caller for testing;
