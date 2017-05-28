5447ea7a9fb162391618493ca845eab1cdce0ff9
Alexandre Fonseca Faria <alexandreffaria@me.com>
2017-05-28 16:18:53 -0300

    Pagination links class active now working but students are not changing when link is clicked now.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ var students = $('.student-item'); // Every student in the page. Used to first p
 var paginationArea = document.querySelector('.pagination'); // Used to first page load.
 
 function loadsPage() { //Loads index page
-  for(i = 10; i < students.length; i++){ // This for just hide everyone on the page, but the first 10 students, using the student-iten class
+  for(var i = 10; i < students.length; i++){ // This for just hide everyone on the page, but the first 10 students, using the student-iten class
     students[i].style.display = 'none';  // Chages the CSS prop 'display' to none
   }
-  var paginationArea = $('.page-header h2::after');
+//  var paginationArea = $('.page-header h2::after');
 
-  paginationArea.innerHTML = '<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>';
+//  paginationArea.innerHTML = '<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>';
     appendPageLinks(students); //Create the pagination links for all the students
 
     $(".pagination li:first-child a").addClass("active"); //sets the link 1 class to active
@@ -33,7 +33,7 @@ function appendPageLinks(listOfStudents) { /* take a student list as an argument
 
     $('.active').removeClass('active'); //Removes every link that has the active class
     $(this).addClass('active'); //Adds the active class to the link clicked
-    showPage(this.text, listOfStudents); //Calls showPage() that print accordingly to the link
+    showPage(this.text(), listOfStudents); //Calls showPage() that print accordingly to the link
 
   })
 };
@@ -41,24 +41,26 @@ function appendPageLinks(listOfStudents) { /* take a student list as an argument
 
 
 
-function showPage(pageNumber, studentsList){ //recieves the link clicked and a list of all the students
+function showPage(pageNumber, listOfStudents){ //recieves the link clicked and a list of all the students
 
   var superiorLimit = pageNumber*10; // The math to position the students to be selected
   var inferiorLimit = superiorLimit - 10;
 
-  for(i = 0; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
+  for(var i = 0; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
+
     students[i].style.display = 'none';
+
   }
 
-   for( ; superiorLimit >= inferiorLimit; superiorLimit--){ // This for display the students on the passed studentsList
+  for( ; superiorLimit > inferiorLimit; superiorLimit--){ // This for display the students on the passed studentsList
 
-     if(!studentsList[superiorLimit]){} // Makes sure the loop skips the complement of 10. Meaning if there is 6 students we wont try to access the other 4 elements that doensnt existe in the array.
+     if(!listOfStudents[superiorLimit]){} // Makes sure the loop skips the complement of 10. Meaning if there is 6 students we wont try to access the other 4 elements that doensnt existe in the array.
 
-     else studentsList[superiorLimit].style.display = 'block'; // Display the students
+     else listOfStudents[superiorLimit].style.display = 'block'; // Display the students
 
    }
-  if (pageNumber === 1){} // if page number = 0 we dont need to show the paginationArea
-  else appendPageLinks(studentsList); // create the paginationArea
+  if (pageNumber === 1){} // if page number = 1 we dont need to show the paginationArea
+  else appendPageLinks(listOfStudents); // create the paginationArea
 
 };
 
@@ -69,6 +71,11 @@ function searchList() { //Rearange the students based on user query
 
     var $studentSearched = $('input').val(); // Search text input
     var studentsFound = []; // array for the students found
+
+    for(var i = 0; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
+      students[i].style.display = 'none';
+    }
+
 
     $('.pagination a').remove(); // Remove pagination, so we can create the new one with the size of the results
 
