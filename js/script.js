515e1bb01a4617012c3eb86215ef9ed6e0e44631
Alexandre Fonseca Faria <alexandreffaria@me.com>
2017-06-07 01:03:11 -0300

    feels good, did not find any other behavioral problem.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 //Pagination & Content Filter
 
 
-var students = $('.student-item'); // Every student in the page. Used to first page load.
-var paginationArea = $('.pagination'); // Used to first page load.
+const students = $('.student-item'); // Every student in the page. Used to first page load.
+const paginationArea = $('.pagination'); // Used to first page load.
+const searchArea = $('.student-search');
+
+function changeVisibility(list, goal) {
+  for(let i = 0; i < list.length; i++){ // This for just hide everyone on the page, but the first 10 students, using the student-iten class
+    students[i].style.display = goal;  // Changes the CSS prop 'display' to none
+  }
+
+};
+
 
 function loadsPage() { //Loads index page
-  for(var i = 10; i < students.length; i++){ // This for just hide everyone on the page, but the first 10 students, using the student-iten class
+  for(let i = 10; i < students.length; i++){ // This for just hide everyone on the page, but the first 10 students, using the student-iten class
     students[i].style.display = 'none';  // Changes the CSS prop 'display' to none
   }
-//  var paginationArea = $('.page-header h2::after');
 
-//  paginationArea.innerHTML = '<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>';
-    appendPageLinks(students); //Create the pagination links for all the students
+  appendPageLinks(students); //Create the pagination links for all the students
+  searchList(); // Loads the searchbox
 
-    $(".pagination li:first-child a").addClass("active"); //sets the link 1 class to active
-
-    searchList(); // Loads the searchbox
 };
 
 function appendPageLinks(listOfStudents) { /* take a student list as an argument */
@@ -38,9 +43,6 @@ function appendPageLinks(listOfStudents) { /* take a student list as an argument
 
   })
 };
-
-
-
 
 function showPage(pageNumber, listOfStudents){ //recieves the link clicked and a list of all the students
 
@@ -65,37 +67,43 @@ function showPage(pageNumber, listOfStudents){ //recieves the link clicked and a
 
 };
 
-
 function searchList() { //Rearange the students based on user query
 
-  $('button').click(function(){ //listens to html button
+  const searchBox = '<input placeholder="Search for students..."></input><button>Search</button>';
+  searchArea.html(searchBox); //inserts the searchArea to the html.
 
-    var $studentSearched = $('input').val(); // Search text input
+  $('button').click(function(){ //listens to html button
+          //debugger;
+    var studentSearched = $('input').val(); // Search text input
     var studentsFound = []; // array for the students found
 
-    for(var i = 0; i < students.length; i++){ // This for just hide everyone on the page using th stunde-iten class
-      students[i].style.display = 'none';
+    if(studentSearched === ''){ // makes sure nothing happens if button is pressed with an empty input and refreshed previous searches
+      changeVisibility(students,'');
+      loadsPage(); // Just refreshes the page if no one is found
     }
-
-
-    $('.pagination a').remove(); // Remove pagination, so we can create the new one with the size of the results
+    else{
+      $('.pagination a').remove(); // Remove pagination, so we can create the new one with the size of the results
 
       for(var i = 0; i < students.length; i++ ){ // Loop for all the students
 
-        if(students[i].innerText.indexOf($studentSearched) >= 0){ //indexOf returns the position in the array of the $studentSearched checking with every student in the page.
+        if(students[i].innerText.indexOf(studentSearched) >= 0){ //indexOf returns the position in the array of the $studentSearched checking with every student in the page.
           studentsFound.push(students[i]); // pushes the student found to an array
         }
       }
 
       if(!studentsFound.length) { //sends a message to the user that no one was found
         alert('No one found :(');
+        changeVisibility(students,'hide');
+        loadsPage(); // Just refreshes the page if no one is found
       }
-      else if(studentsFound.length > 10){ //checks if we need more pages
+      else if(studentsFound.length > 10){ //checks if we need pages
         appendPageLinks(studentsFound);
       }
       else{ // case for students found but we dont need more the one page.
         showPage(1,studentsFound);
       }
+  }
+
 
   });
 
